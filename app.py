c087b02d62f90a210c127e64496f86d416178b32
Yasser Saleemi <yassersaleemi@gmail.com>
2026-03-04 21:27:16 +0000

    fix: Format app.py with ruff 0.1.14

--- app.py
+++ app.py
@@ -54,9 +54,9 @@ def nocache(view: Callable) -> Callable:
     def no_cache(*args: Any, **kwargs: Any) -> Any:
         response = make_response(view(*args, **kwargs))
         response.headers["Last-Modified"] = datetime.now()
-        response.headers["Cache-Control"] = (
-            "no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0"
-        )
+        response.headers[
+            "Cache-Control"
+        ] = "no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0"
         response.headers["Pragma"] = "no-cache"
         response.headers["Expires"] = "-1"
         return response
